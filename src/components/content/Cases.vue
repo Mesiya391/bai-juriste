acb5b22ab981d48751f3f579fccdb89dd8696d71
BlackAndWhiteZebra <i.babik@onet.eu>
2020-04-29 22:27:31 +0200

    created dropdown

--- src/components/content/Cases.vue
+++ src/components/content/Cases.vue
@@ -1,15 +1,67 @@
 <template>
     <div>
-        <h2>TUTAJ BEDZIE CASE OVERVIEW Z EVENTAMI, TERMINAMI, PISMAMI i NOTATTKAMI</h2>
+        <label>Browser Select</label>
+        <select class="browser-default" v-model="selectedCase">
+            <option value="" disabled selected>Choose your option</option>
+            <option v-for="item in caseList" :value="item" v-bind:key="item">{{item}}</option>
+        </select>
+        <button @click="displayCaseInfo" class="btn deep-purple">Wybierz</button>
     </div>
 </template>
 
 <script>
+    import firebase from "firebase";
+    import db from '@/firebase/init';
     export default {
-        name: 'Cases'
+        name: 'Cases',
+        data() {
+            return {
+                caseList: [],
+                selectedCase: null
+            }
+        },
+        mounted() {
+            if(this.selectedCase){
+                console.log("Mounted is working")
+            }
+        },
+        methods: {
+            displayCaseInfo(){
+              //  let user = firebase.auth().currentUser;
+               // let userID = user.uid
+                //let ref = db.collection('cases').doc(userID).collection('userCases').doc(this.selectedCase)
+            }
+        },
+        computed: {
+            options(){
+                return Object.keys(this.clients).map(k => {
+                    let o = this.clients[k]
+                    return `${o.first_name} ${o.middle_name ? o.middle_name : ''} ${o.last_name}`
+                })
+            },
+        },
+        created() {
+            let caseList = this.caseList
+            let user = firebase.auth().currentUser;
+            let userID = user.uid
+            db.collection('cases').doc(userID).collection('userCases')
+                .get()
+                .then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc) {
+                        // doc.data() is never undefined for query doc snapshots
+                        caseList.push(doc.id)
+                        console.log(doc.id, " => ", doc.data(), caseList);
+                    });
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                });
+
+        }
     }
 </script>
 
 <style>
+
 
 </style>
