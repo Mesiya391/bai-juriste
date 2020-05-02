3417db3506009b3b55cbfe25516a1ed9eef4e472
BlackAndWhiteZebra <i.babik@onet.eu>
2020-05-02 02:58:24 +0200

    upgraded delete section

--- src/components/content/Delete.vue
+++ src/components/content/Delete.vue
@@ -2,11 +2,42 @@
     <div>
         <router-link :to="{ name : 'DeleteCase'}">Delete Case</router-link>
         <router-link :to="{ name : 'DeleteCaseElement'}">Delete Case Element</router-link>
+        <div class="container">
+            <label>Wybierz akcję</label>
+            <select class="browser-default" @change="displayForm" v-model="action">
+                <option value="" disabled selected>Choose your option</option>
+                <option  :value="'DeleteCase'" >Usuń sprawę</option>
+                <option  :value="'DeleteCaseElement'" >Usuń element sprawy</option>
+            </select>
+        </div>
+        <DeleteCase id="DeleteCase" hidden />
+        <DeleteCaseElement id="DeleteCaseElement" hidden />
     </div>
 </template>
 
 <script>
+    import DeleteCase from "../delete-forms/DeleteCase";
+    import DeleteCaseElement from "../delete-forms/DeleteCaseElement";
     export default {
-        name: 'Delete'
+        name: 'Delete',
+        components: {
+            DeleteCase,
+            DeleteCaseElement
+        },
+        methods: {
+            displayForm: function(){
+                this.hideAllForms()
+                if(this.action == 'DeleteCase'){
+                    document.getElementById('DeleteCase').hidden = false
+                }else if(this.action == 'DeleteCaseElement'){
+                    document.getElementById('DeleteCaseElement').hidden = false
+                }
+            },
+            hideAllForms: function(){
+                document.getElementById('DeleteCase').hidden = true
+                document.getElementById('DeleteCaseElement').hidden = true
+            }
+        }
+
     }
 </script>
