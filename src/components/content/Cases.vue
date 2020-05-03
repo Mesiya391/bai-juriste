b32b40b1d49cf7102b8c53281b05314417e0eef4
BlackAndWhiteZebra <i.babik@onet.eu>
2020-05-03 19:59:12 +0200

    tools and cases edit

--- src/components/content/Cases.vue
+++ src/components/content/Cases.vue
@@ -343,7 +343,6 @@
                 .get()
                 .then(function(querySnapshot) {
                     querySnapshot.forEach(function(doc) {
-                        // doc.data() is never undefined for query doc snapshots
                         caseList.push(doc.id)
                         console.log(doc.id, " => ", doc.data(), caseList);
                     });
